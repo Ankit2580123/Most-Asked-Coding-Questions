d84fc52dbb4276590a76abe60f664579a2bb6b31
Ankit2580123 <ankitkumar2580123@gmail.com>
2025-01-05 22:45:52 +0530

    update nineteen.py

--- nineteen.py
+++ nineteen.py
@@ -15,7 +15,9 @@ new_dict=sorted_series.to_dict()
 print(new_dict)
 
 #approach 2-->use sorted and Lambda function
-
-ans=dict(sorted(dicts.items(),key=lambda x: x[1]))
-print(ans)
+print(dicts.items())
+ans1=dict(sorted(dicts.items(),key=lambda x: x[1]))  #sort on the basis of values
+print(ans1)
+ans2=dict(sorted(dicts.items(),key=lambda x: x[0]))  #sort on the basis of keys
+print(ans2)
 
