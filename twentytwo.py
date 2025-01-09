cbade62ca3840990e3602da78da228b5ba4eba6b
Ankit2580123 <ankitkumar2580123@gmail.com>
2025-01-09 19:08:17 +0530

    update twentytwo.py

--- twentytwo.py
+++ twentytwo.py
@@ -2,28 +2,42 @@
 
 list=[1,2,35,4,6,8]  ##-->third Largest-->6
 
-def third_largest(list):
-    third_largest=float('-inf')
-    first_largest=float('-inf')
-    second_largest=float('-inf')
+
+def finding_third_largest(list):
+    length=len(list)
+    for i in range(length):
+        for j in range(length-i-1):
+            if list[j]>list[j+1]:
+                temp=list[j]
+                list[j]=list[j+1]
+                list[j+1]=temp
+    return list[-3]
+
+output=finding_third_largest(list)
+print(output)
+
+# def third_largest(list):
+#     third_largest=float('-inf')
+#     first_largest=float('-inf')
+#     second_largest=float('-inf')
     
-    for i in list:
-        if i>first_largest:
-            third_largest=second_largest
-            second_largest=first_largest
-            first_largest=i
+#     for i in list:
+#         if i>first_largest:
+#             third_largest=second_largest
+#             second_largest=first_largest
+#             first_largest=i
             
-        elif i>second_largest  and i!=first_largest:
-            third_largest=second_largest
-            second_largest=i
+#         elif i>second_largest  and i!=first_largest:
+#             third_largest=second_largest
+#             second_largest=i
 
-        elif  i>third_largest and i!=second_largest and i!=first_largest:
-            third_largest=i
+#         elif  i>third_largest and i!=second_largest and i!=first_largest:
+#             third_largest=i
 
-    return third_largest
+#     return third_largest
 
-ans=third_largest(list)
-print(ans)
+# ans=third_largest(list)
+# print(ans)
 
 
 
