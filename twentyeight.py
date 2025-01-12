5592ae336f0856199211651aace4e0585290fdf7
Ankit2580123 <ankitkumar2580123@gmail.com>
2025-01-12 11:24:33 +0530

    update twentyeight.py

--- twentyeight.py
+++ twentyeight.py
@@ -1,3 +1,12 @@
+#Tricky Interview Questions--01
+a = [1, 2, 3]
+b = a  #[1,2,3]
+# print(a is b)
+b.append(4) #[1,2,3,4]
+print(a)  #[1,2,3,4]
+print(b)  #[1,2,3,4]
+
+
 #Tricky Interview Questions--02
 x = 10
 def modify_var():
@@ -8,10 +17,31 @@ modify_var()
 print(x)  ##Output-->15
 
 
-#Tricky Interview Questions--01
-a = [1, 2, 3]
-b = a  #[1,2,3]
-# print(a is b)
-b.append(4) #[1,2,3,4]
-print(a)  #[1,2,3,4]
-print(b)  #[1,2,3,4]
+#Tricky Interview Questions--03
+list = ['a', 'b', 'c', 'd', 'e']
+print(list[10:])   #print empty because starting point not available
+
+#Tricky Interview Questions--04
+list = [ [ ] ] * 5 
+print(list)  # output?  #-->[[],[],[],[],[]]
+list[0].append(10)
+print(list)  # output?  [[10], [10], [10], [10], [10]]
+list[1].append(20)
+print(list)  # output?  [[10, 20], [10, 20], [10, 20], [10, 20], [10, 20]]
+list.append(30)
+print(list)  # output?  [[10, 20], [10, 20], [10, 20], [10, 20], [10, 20], 30]
+
+#Tricky Interview Questions--05
+def main(a):
+    a = a + '2'  #byte2
+    a = a * 2    #byte2byte2
+    return a
+print(main("byte"))
+
+#Tricky Interview Questions--06
+i = 1
+while True:
+    if i % 3 == 0:
+        break
+    print(i)  # 1,2
+    i += 1    # 2,3
