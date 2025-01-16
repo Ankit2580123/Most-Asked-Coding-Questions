f23c9721bb0bec49431c61f5f360c2d060ae159f
Ankit2580123 <ankitkumar2580123@gmail.com>
2025-01-16 21:50:39 +0530

    update 32.py

--- 32.py
+++ 32.py
@@ -1,16 +1,24 @@
 #Write a program to remove white spaces of the string
 
-def remove_spaces(str_input):
-    updated_str_input=str_input.strip()
-    ans=""
-    for i in updated_str_input:
-        if i!=' ':
-            ans+=i
-        else:
-            continue
+
+txt = ",,,,,rrttgg.....Lion....rrr"
+
+x = txt.strip(",.grt")
+
+print(x)
+
+
+# def remove_spaces(str_input):
+#     updated_str_input=str_input.strip()  #remove white spaces from both ends 
+#     ans=""
+#     for i in updated_str_input:
+#         if i!=' ':
+#             ans+=i
+#         else:
+#             continue
     
-    print(ans)
+#     print(ans)
 
 
-str_input=" Ank i t Kuma r"
-final_output=remove_spaces(str_input)
+# str_input=" Ank i t Kuma r"
+# final_output=remove_spaces(str_input)
